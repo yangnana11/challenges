3f4fa6e586470cdafaaafc3a2e28c0180a692d1e
ㅡ <eu@euui-MacBook-Air.local>
2020-03-06 18:49:54 -0600

    odd number of times

---
+++ codewars/csharp/6kyu/find_it.cs
@@ -0,0 +1,22 @@
+using System;
+namespace Solution
+{
+  class Kata
+    {
+    public static int find_it(int[] seq) 
+      {
+        for (int i = 0; i < seq.Length; i++) { 
+            int count = 0; 
+              
+            for (int j = 0; j < seq.Length; j++) { 
+                if (seq[i] == seq[j]) 
+                    count++; 
+            } 
+            if (count % 2 != 0) 
+                return seq[i]; 
+        } 
+        return -1; 
+      }
+       
+    }
+}
