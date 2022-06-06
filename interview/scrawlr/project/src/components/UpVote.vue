a91962b6c52f1f471a20348f98fdedb43c9ff7d8
yangnana11 <yphan1988@gmail.com>
2022-06-06 01:15:13 -0400

    upvote init code

---
+++ interview/scrawlr/project/src/components/UpVote.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="changeStatus">{{ status }}</button>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: mapGetters([
+    'status'
+  ]),
+  methods: mapActions([
+    'changeStatus'
+  ])
+}
+</script>
