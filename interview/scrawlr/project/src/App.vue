5867062f16cf66360f1fd6918dd13342dbd8833d
yangnana11 <yphan1988@gmail.com>
2022-06-06 01:32:29 -0400

    dynamic component

--- interview/scrawlr/project/src/App.vue
+++ interview/scrawlr/project/src/App.vue
@@ -2,11 +2,23 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <UpVote msg="Hello Vue 3.0 + Vite" /> -->
   <component
-     v-for="(item,index) in children"
+     v-for="(item,index) in children1"
      :is="item"
      :key="index"
     ></component>
-    <button @click="addComponent()">Add</button>
+    <button @click="addComponent1()">Add</button>
+    <component
+     v-for="(item,index) in children2"
+     :is="item"
+     :key="index"
+    ></component>
+    <button @click="addComponent2()">Add</button>
+    <component
+     v-for="(item,index) in children3"
+     :is="item"
+     :key="index"
+    ></component>
+    <button @click="addComponent3()">Add</button>
 </template>
 
 <script>
@@ -16,15 +28,23 @@ export default {
   name: 'App',
   data() {
     return {
-      children: []
+      children1: [],
+      children2:[],
+      children3:[]
     }
   },
   components: {
     UpVote
   },
   methods: {
-    addComponent() {
-      this.children.push(UpVote)
+    addComponent1() {
+      this.children1.push(UpVote)
+    },
+    addComponent2() {
+      this.children2.push(UpVote)
+    },
+    addComponent3() {
+      this.children3.push(UpVote)
     }
   }
 }
