bc0690385aea13a58cdbc98d13866c3b8965fc35
yangnana11 <yphan1988@gmail.com>
2024-01-22 23:03:41 -0500

    add hackerrank section

---
+++ interview/gg/shortestPath.js
@@ -0,0 +1,8 @@
+/**
+ * There is a 1D maze, a path is from first elemenent to last element
+ * We can move forward 1 index, or to the postion same value as current index
+ * Find a shortest path in the maze
+ * 
+ * for examle maze: 1,6,1,2,3,4,5,6
+ * path will be: 0 1 7 (index of 1, 6, 6)
+ */
